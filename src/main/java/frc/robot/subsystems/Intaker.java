5b71faa67410e1c750486a0af4a66e566b28f1a2
Ryan Shillington <ryan@qbdvision.com>
2023-04-13 21:38:07 -0400

    INTAKE FIXED

--- src/main/java/frc/robot/subsystems/Intaker.java
+++ src/main/java/frc/robot/subsystems/Intaker.java
@@ -4,7 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.robot.Constants;
+import frc.robot.Constants;
 //import frc.robot.Constants.IntakeConstants;
 
 
@@ -13,7 +13,7 @@ public class Intaker extends SubsystemBase {
     /*Config motors and encoders */
     //private final CANSparkMax m_intake = new CANSparkMax(Constants.IntakeConstants.Mod4.intakeMotorID,MotorType.kBrushless);
     //private Encoder intakeEncoder = new Encoder(Constants.IntakeConstants.Mod4.intakeMotorID1, Constants.IntakeConstants.Mod4.intakeMotorID2, Constants.IntakeConstants.Mod4.kEncoderReversed);
-    private final CANSparkMax intakeMotor = new CANSparkMax(13, MotorType.kBrushless);
+    private final CANSparkMax intakeMotor = new CANSparkMax(Constants.IntakeConstants.Mod4.intakeMotorID, MotorType.kBrushless);
 
     // turn on the intake motor to pull in objects.
     public void pull() {
