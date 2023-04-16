bcfe5efd578c9b241a8cce02f4099bd5e18ba43e
Ryan Shillington <ryan@qbdvision.com>
2023-04-16 17:21:29 -0400

    Autonomous commands for the different spots on the field.

--- src/main/java/frc/robot/autos/BusterAuto.java
+++ src/main/java/frc/robot/autos/BusterAuto.java
@@ -21,31 +21,53 @@ import com.pathplanner.lib.PathPoint;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class BusterAuto extends SequentialCommandGroup {
   private RobotContainer m_robotContainer;
+
   public BusterAuto(RobotContainer container, SendableChooser<String> chooser) {
     m_robotContainer = container;
-   
     addCommands(
-        // Lift the lifter 4 rotations
-        // new InstantCommand(() -> m_robotContainer.s_Lifter.setToPosition(4)),
-        // new WaitCommand(5),
-        // Push out the cube using the intaker
-        // new InstantCommand(() -> m_robotContainer.s_Intaker.push()),
-        // new WaitCommand(2),
-        // new InstantCommand(() -> m_robotContainer.s_Intaker.stop()),
-
-        // Move backward
-        new InstantCommand(() -> m_robotContainer.s_Swerve.drive(new Translation2d(1, 0), 0, false, true)),
+        new InstantCommand(() -> m_robotContainer.s_Intaker.push()),
         new WaitCommand(3),
+        new InstantCommand(() -> m_robotContainer.s_Intaker.stop()));
+    switch (chooser.getSelected()) {
+      case "strait":
+        addCommands(
+            new InstantCommand(() -> m_robotContainer.s_Swerve.drive(new Translation2d(4.5, 0), 0, false, true)),
+            new WaitCommand(3),
+            new InstantCommand(() -> m_robotContainer.s_Swerve.drive(new Translation2d(0, 0), 0, false, true))
+        );
+        break;
+      case "left":
+      addCommands(
+      new InstantCommand(() -> m_robotContainer.s_Swerve.drive(new Translation2d(0, 4.5), 0, false, true)),
+      new WaitCommand(3),
+      new InstantCommand(() -> m_robotContainer.s_Swerve.drive(new Translation2d(4.5, 0), 0, false, true)),
+      new WaitCommand(3),
+      new InstantCommand(() -> m_robotContainer.s_Swerve.drive(new Translation2d(0, 0), 0, false, true))
+    
+      );
+      break;
+      case "right":
+      addCommands(
+      new InstantCommand(() -> m_robotContainer.s_Swerve.drive(new Translation2d(0, -4.5), 0, false, true)),
+      new WaitCommand(3),
+      new InstantCommand(() -> m_robotContainer.s_Swerve.drive(new Translation2d(4.5, 0), 0, false, true)),
+      new WaitCommand(3),
+      new InstantCommand(() -> m_robotContainer.s_Swerve.drive(new Translation2d(0, 0), 0, false, true))
+  
+      );
+        break;
+      case "none":
+        break;
 
-        new InstantCommand(() -> m_robotContainer.s_Swerve.drive(new Translation2d(0, 0), 0, false, true))
-    // Stop the robot
+    }
+    addCommands(
+    // left
 
     );
-
+    addCommands();
   }
 
 }
