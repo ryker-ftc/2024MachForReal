35b77febf2e0f0ee5113fb1c5f868893bbc93671
Hyojin No <hyojinc.no@gmail.com>
2024-01-08 20:21:33 -0500

    make class

--- src/main/java/frc/robot/subsystems/GroundIntake.java
+++ src/main/java/frc/robot/subsystems/GroundIntake.java
@@ -1,50 +1,6 @@
 //akul 
-//package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-// import edu.wpi.first.wpilibj.DigitalInput;
-// import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.Constants;
-// //import frc.robot.Constants.IntakeConstants;
-
-
-// public class GroundIntake extends SubsystemBase {
-
-//     /*Config motors and encoders */
-//     //private final CANSparkMax m_intake = new CANSparkMax(Constants.IntakeConstants.Mod4.intakeMotorID,MotorType.kBrushless);
-//     //private Encoder intakeEncoder = new Encoder(Constants.IntakeConstants.Mod4.intakeMotorID1, Constants.IntakeConstants.Mod4.intakeMotorID2, Constants.IntakeConstants.Mod4.kEncoderReversed);
-//     //private final CANSparkMax intakeMotor = new CANSparkMax(13, MotorType.kBrushless);
-//     private final PWMSparkMax intakeMotor = new PWMSparkMax(6);
-  
-//     public void periodic() {
-//         SmartDashboard.putNumber("Intake speed", intakeMotor.get());
-//     }
-
-//     // turn on the intake motor to pull in objects.
-//     public void pull() {
-//         SmartDashboard.putString("Intake Status", "PULL");
-//         //intakeMotor.setInverted(false);
-//         intakeMotor.set(1);
-//     }
-
-//     // turn on the intake motor to push out objects.
-//     public void push() {
-//         SmartDashboard.putString("Intake Status", "PUSH");
-//         // intakeMotor.setInverted(true);
-//         intakeMotor.set(-1.0);
-//     }
-
-//     // this is the default state of the intake motor - do not move.
-//     public void stop() {
-//         SmartDashboard.putString("Intake Status", "STOP");
-//         intakeMotor.set(0);
-//     }
-
+public class GroundIntake{
     
-
-
-// }
+}
--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -1,58 +1,6 @@
 //tej
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// //import edu.wpi.first.wpilibj.XboxController;
-// import com.revrobotics.CANSparkMax;
-// //import com.revrobotics.RelativeEncoder;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-// public class Lifter extends SubsystemBase {
-//     /* Config motors and encoders */
-//     // private final CANSparkMax m_intake = new
-//     // CANSparkMax(Constants.IntakeConstants.Mod4.intakeMotorID,MotorType.kBrushless);
-//     // private Encoder intakeEncoder = new
-//     // Encoder(Constants.IntakeConstants.Mod4.intakeMotorID1,
-//     // Constants.IntakeConstants.Mod4.intakeMotorID2,
-//     // Constants.IntakeConstants.Mod4.kEncoderReversed);
-//     private CANSparkMax lifterMotor;
-
-//     public Lifter() {
-//         lifterMotor = new CANSparkMax(14, MotorType.kBrushless);
-//     }
-
-//     public void junk() {
-//         lifterMotor.getPIDController().getP();
-//         lifterMotor.getPIDController().getI();
-//         lifterMotor.getPIDController().getD();
-//         SmartDashboard.putNumber("P value", lifterMotor.getPIDController().getP());
-//         SmartDashboard.putNumber("I value", lifterMotor.getPIDController().getI());
-//         SmartDashboard.putNumber("D value", lifterMotor.getPIDController().getD());
-//     }
-
-//     // turn on the intake motor to pull in objects.
-//     public void push() {
-//         lifterMotor.set(0.25);
-//         SmartDashboard.putString("Lifter Status", "PUSHING");
-//     }
-
-//     // turn on the intake motor to push out objects.
-//     public void pull() {
-//         lifterMotor.set(-0.25);
-//         SmartDashboard.putString("Lifter Status", "PULLING");
-//     }
-
-//     // this is the default state of the intake motor - do not move.
-//     public void stop() {
-//         lifterMotor.set(0);
-//         SmartDashboard.putString("Lifter Status", "STOPPED");
-//     }
-
-//     public Object setToPosition(int position) {
-//         return null;
-//     }
-
-//     public void checkLimits() {
-//     }
-// }
+public class Outtake{
+    
+}
