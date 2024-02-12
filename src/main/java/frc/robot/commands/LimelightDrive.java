d78c5ad556dc17b9e31cece8ed2a7f00d2c40eb3
Pranav Kulkarni <pranavkulkarni2104@gmail.com>
2024-02-12 18:19:30 -0500

    added methods to LimelightDrive

--- src/main/java/frc/robot/commands/LimelightDrive.java
+++ src/main/java/frc/robot/commands/LimelightDrive.java
@@ -1,8 +1,9 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.*;
 
-public class LimelightDrive {
+public class LimelightDrive extends CommandBase{ 
     private Camera m_camera;
     private Swerve m_swerve;
     private TurnToAngleCommand c_turnToAngleCommand;
@@ -11,6 +12,16 @@ public class LimelightDrive {
         m_camera = camera;
         m_swerve  = swerve;
         c_turnToAngleCommand = new TurnToAngleCommand(swerve, 0, 0);
+    }
+
+    public void execute() {}    
+
+    public boolean isFinished() {
+        return false;
+    }
+
+    public void end(boolean interrupted) {
+
     }
     
 }
