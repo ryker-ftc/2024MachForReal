d870e1f8ae5e52c1800a6f376c332ccbc6774c07
Tej Parikh <tejdparikh@gmail.com>
2024-02-25 15:49:04 -0500

    updated to 2024

--- src/main/java/frc/robot/autos/BusterAuto.java
+++ src/main/java/frc/robot/autos/BusterAuto.java
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import frc.robot.Constants.AutoConstants;
-import frc.robot.commands.TurnToAngleCommand;
 // import frc.robot.subsystems.Intaker;
 import frc.robot.subsystems.Swerve;
 
