29cf828a398475f97924b04971f0e1bf5827b4c2
Tej Parikh <tejdparikh@gmail.com>
2024-03-31 17:10:19 -0400

    added bottom intake

--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -22,7 +22,8 @@ public class Conveyor extends SubsystemBase {
     //private Encoder intakeEncoder = new Encoder(Constants.IntakeConstants.Mod4.intakeMotorID1, Constants.IntakeConstants.Mod4.intakeMotorID2, Constants.IntakeConstants.Mod4.kEncoderReversed);
     //private final CANSparkMax intakeMotor = new CANSparkMax(13, MotorType.kBrushless);
     //private final PWMSparkMax intakeMotor = new PWMSparkMax(6);
-    private final CANSparkMax intakeMotor;
+    private final CANSparkMax intakeMotorTop;
+    private final CANSparkMax intakeMotorBottom;
     private final CANSparkMax placementMotor;
     private final CANSparkMax flywheelMotorLeft;
     private final CANSparkMax flywheelMotorRight;
@@ -30,7 +31,8 @@ public class Conveyor extends SubsystemBase {
     private final TCSColor noteColor = new TCSColor(0, 0, 0, 0);
 
     public Conveyor() {
-        intakeMotor = new CANSparkMax(Mod4.intakeMotorID, MotorType.kBrushless);
+        intakeMotorTop = new CANSparkMax(Mod4.intakeMotorID, MotorType.kBrushless);
+        intakeMotorBottom = new CANSparkMax(Mod4.intakeMotorID, MotorType.kBrushless);
         placementMotor = new CANSparkMax(Mod4.placementMotorID, MotorType.kBrushless);
         flywheelMotorLeft = new CANSparkMax(Mod4.flywheelMotorIDLeft, MotorType.kBrushless);
         flywheelMotorRight = new CANSparkMax(Mod4.flywheelMotorIDRight, MotorType.kBrushless);
@@ -40,22 +42,26 @@ public class Conveyor extends SubsystemBase {
 
 
     public void periodic() {
-        SmartDashboard.putNumber("Intake speed", intakeMotor.get());
+        // SmartDashboard.putNumber("Intake speed", intakeMotor.get());
     }
 
 
     public void groundIntake() {
         TCSColor color = colorSensor.readColors();
         SmartDashboard.putString("Conveyor Status", "GROUNDINTAKE");
-        if (!color.equals(noteColor)){
-            intakeMotor.set(1);
-            placementMotor.set(0.4);
-        } 
+        // if (!color.equals(noteColor)){
+        //     intakeMotor.set(1);
+        //     placementMotor.set(0.4);
+        // }
+        intakeMotorTop.set(0.7);
+        intakeMotorBottom.set(-0.7);
+        placementMotor.set(0.4);
     }
 
     public void groundOuttake() {
         SmartDashboard.putString("Conveyor Status", "GROUNDOUTTAKE");
-        intakeMotor.set(-0.5);
+        intakeMotorTop.set(-0.5);
+        intakeMotorBottom.set(0.5);
         placementMotor.set(-0.2);
     }
 
@@ -81,7 +87,8 @@ public class Conveyor extends SubsystemBase {
     // this is the default state of the intake motor - do not move.
     public void stop() {
         SmartDashboard.putString("Conveyor Status", "STOP");
-        intakeMotor.set(0);
+        intakeMotorTop.set(0);
+        intakeMotorBottom.set(0);
         placementMotor.set(0);
         flywheelMotorLeft.set(0);
         flywheelMotorRight.set(0);
